10201983d320d7446bbfb84f547ae651e298c1fa
Roman <4464530+roman8128@users.noreply.github.com>
2025-01-29 15:40:32 +0100

    initial commit with jpa and graphql config

---
+++ java/com/graphql/demo/QbeApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbeApplication.class, args);
+	}
+
+}
